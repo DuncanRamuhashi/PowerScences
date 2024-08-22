8311796c95c1732a722c498b04ec9d20b50108eb
Duncan Ramuhashi <duncanramohashi@gmail.com>
2024-08-22 15:35:58 +0200

    Finally Finished

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -11,12 +11,12 @@ const Heading = () => {
 <div className="bg-[#f9e3ce] py-4 w-full">
   <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-6">
     
-    <a href="" className="mb-4 md:mb-0">
+    <a href="/" className="mb-4 md:mb-0">
       <img src={logo} alt="Logo" className="w-48 md:w-64 h-auto" />
     </a>
     
     <div className="hidden md:flex md:flex-col  md:space-y-0 md:space-x-8 items-center">
-  <a href="" className="text-green-700 text-lg hover:text-yellow-500">Home</a>
+  <a href="/" className="text-green-700 text-lg hover:text-yellow-500">Home</a>
 </div>
 
   </div>
